cc7b0300ed1a1e8a3635b3e753b6e285bbb28fb2
bouhartsev <m3773517@gmail.com>
2021-09-16 02:24:06 +0300

    Fix path problems

--- www/index.php
+++ www/index.php
@@ -1,11 +1,11 @@
 <?php
  
 spl_autoload_register(function (string $className) {
-    require_once '../src/' . $className . '.php';
+    require_once __DIR__ . '/../src/' . str_replace('\\','/',$className) . '.php';
 });
  
 $route = $_GET['route'] ?? '';
-$routes = require '../src/routes.php';
+$routes = require __DIR__ . '/../src/routes.php';
  
 $isRouteFound = false;
 foreach ($routes as $pattern => $controllerAndAction) {
