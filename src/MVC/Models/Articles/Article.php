b8273d668cbefcff3555921d13269b97ef4eb64e
bouhartsev <m3773517@gmail.com>
2021-09-23 11:30:17 +0300

    Change user

--- src/MVC/Models/Articles/Article.php
+++ src/MVC/Models/Articles/Article.php
@@ -36,49 +36,10 @@ class Article extends ActiveRecordEntity
     {
         $this->text = $text;
     }
-    
+
     public function setAuthor(User $user): void
     {
         $this->authorId = $user->getId();
-    }
-
-    public static function createFromArray(array $fields, User $author): Article
-    {
-        if (empty($fields['name'])) {
-            throw new InvalidArgumentException('Не передано название статьи');
-        }
-
-        if (empty($fields['text'])) {
-            throw new InvalidArgumentException('Не передан текст статьи');
-        }
-
-        $article = new Article();
-
-        $article->setAuthor($author);
-        $article->setName($fields['name']);
-        $article->setText($fields['text']);
-
-        $article->save();
-
-        return $article;
-    }
-
-    public function updateFromArray(array $fields): Article
-    {
-        if (empty($fields['name'])) {
-            throw new InvalidArgumentException('Не передано название статьи');
-        }
-
-        if (empty($fields['text'])) {
-            throw new InvalidArgumentException('Не передан текст статьи');
-        }
-
-        $this->setName($fields['name']);
-        $this->setText($fields['text']);
-
-        $this->save();
-
-        return $this;
     }
 
     protected static function getTableName(): string
