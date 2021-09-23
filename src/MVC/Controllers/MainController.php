578304246cad8751fd53954e4a1d572114502dc9
bouhartsev <m3773517@gmail.com>
2021-09-23 12:19:45 +0300

    findAll

--- src/MVC/Controllers/MainController.php
+++ src/MVC/Controllers/MainController.php
@@ -2,7 +2,6 @@
 
 namespace MVC\Controllers;
 
-use MVC\Services\DB;
 use MVC\Views\MainView;
 
 use MVC\Models\Articles\Article;
@@ -20,25 +19,9 @@ class MainController
  
     public function main()
     {
-        $articles = $this->db->query('SELECT * FROM `articles`;');
-        // $articles = [
-        //     ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-        //     ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
-        // ];
-
+        $articles = Article::findAll();
         $this->view->render('main/main.php', ['articles' => $articles]);
     }
- 
-    public function sayHello(string $name)
-    {
-        $this->view->renderHtml('main/hello.php', ['username' => $name, 'title' => 'Страница приветствия']);
-    }
-    
-    public function sayBye(string $name)
-    {
-        echo 'Bye, ' . $name . '!';
-    }
-    
 }
 
 ?>
--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,12 @@
 <?php
  
 return [
-    '~^hello/(.*)$~' => [\MVC\Controllers\MainController::class, 'sayHello'],
-    '~^bye/(.*)$~' => [\MVC\Controllers\MainController::class, 'sayBye'],
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
+    '~^articles/(\d+)/edit$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
+    '~^articles/add$~' => [\MyProject\Controllers\ArticlesController::class, 'add'],
+    '~^users/register$~' => [\MyProject\Controllers\UsersController::class, 'signUp'],
+    '~^users/(\d+)/activate/(.+)$~' => [\MyProject\Controllers\UsersController::class, 'activate'],
+    '~^users/login$~' => [\MyProject\Controllers\UsersController::class, 'login'],
     '~^$~' => [\MVC\Controllers\MainController::class, 'main'],
 ];
 
