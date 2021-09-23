0089cb8e586b20535c3fc3e87eaf0704b484a759
bouhartsev <m3773517@gmail.com>
2021-09-23 11:27:03 +0300

    Change styles structure and add parent class

--- src/MVC/Controllers/MainController.php
+++ src/MVC/Controllers/MainController.php
@@ -4,18 +4,18 @@ namespace MVC\Controllers;
 
 use MVC\Services\DB;
 use MVC\Views\MainView;
+
+use MVC\Models\Articles\Article;
  
 class MainController
 {
     private $db;
-    private $template;
-    private $style;
+    private $view;
  
     public function __construct()
     {
         $this->db = new DB();
-        $this->template = new MainView(__DIR__ . '/../../templates');
-        $this->style = new MainView(__DIR__ . '/../../styles');
+        $this->view = new MainView(__DIR__ . '/../../templates');
     }
  
     public function main()
@@ -26,17 +26,12 @@ class MainController
         //     ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
         // ];
 
-        $this->template->render('main/main.php', ['articles' => $articles]);
-    }
-
-    public function styles(string $file)
-    {
-        $this->style->render($file);
+        $this->view->render('main/main.php', ['articles' => $articles]);
     }
  
     public function sayHello(string $name)
     {
-        $this->template->renderHtml('main/hello.php', ['username' => $name, 'title' => 'Страница приветствия']);
+        $this->view->renderHtml('main/hello.php', ['username' => $name, 'title' => 'Страница приветствия']);
     }
     
     public function sayBye(string $name)
--- src/MVC/Models/Articles/Article.php
+++ src/MVC/Models/Articles/Article.php
@@ -1,34 +1,88 @@
 <?php
- 
+
 namespace MVC\Models\Articles;
- 
+
+use MVC\Models\ActiveRecordEntity;
 use MVC\Models\Users\User;
- 
-class Article
+
+class Article extends ActiveRecordEntity
 {
-    private $title;
-    private $text;
-    private $author;
- 
-    public function __construct(string $title, string $text, User $author)
+    protected $name;
+    protected $text;
+    protected $authorId;
+    protected $createdAt;
+
+    public function getName(): string
     {
-        $this->title = $title;
-        $this->text = $text;
-        $this->author = $author;
+        return $this->name;
     }
- 
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
- 
+
     public function getText(): string
     {
         return $this->text;
     }
- 
+
     public function getAuthor(): User
     {
-        return $this->author;
+        return User::getById($this->authorId);
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+    
+    public function setAuthor(User $user): void
+    {
+        $this->authorId = $user->getId();
+    }
+
+    public static function createFromArray(array $fields, User $author): Article
+    {
+        if (empty($fields['name'])) {
+            throw new InvalidArgumentException('Не передано название статьи');
+        }
+
+        if (empty($fields['text'])) {
+            throw new InvalidArgumentException('Не передан текст статьи');
+        }
+
+        $article = new Article();
+
+        $article->setAuthor($author);
+        $article->setName($fields['name']);
+        $article->setText($fields['text']);
+
+        $article->save();
+
+        return $article;
+    }
+
+    public function updateFromArray(array $fields): Article
+    {
+        if (empty($fields['name'])) {
+            throw new InvalidArgumentException('Не передано название статьи');
+        }
+
+        if (empty($fields['text'])) {
+            throw new InvalidArgumentException('Не передан текст статьи');
+        }
+
+        $this->setName($fields['name']);
+        $this->setText($fields['text']);
+
+        $this->save();
+
+        return $this;
+    }
+
+    protected static function getTableName(): string
+    {
+        return 'articles';
     }
 }
